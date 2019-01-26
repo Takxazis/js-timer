194a76296ef2ad344b1c82152d9bf91c4e55e8a0
Takxazis <44546443+Takxazis@users.noreply.github.com>
2019-01-26 11:17:35 +0200

    Update dz.js

--- dz.js
+++ dz.js
@@ -1,26 +1,58 @@
+let msec = 0;
 let sec = 0;
+let min = 0;
+let hour = 0;
+
+
 
 const Hstart = document.getElementById('HTMLstart').addEventListener("click", start);
 const Hclear = document.getElementById('HTMLclear').addEventListener("click", clear);
 const Hstop = document.getElementById('HTMLstop').addEventListener("click", stop);
 
+// T = text;
+
+const hourT = document.getElementById('HTMLhour');
+const minT = document.getElementById('HTMLmin');
+const secT = document.getElementById('HTMLsec');
+const msecT = document.getElementById('HTMLmsec');
+
+
+
+
 function start() {
     timer = setInterval(function () {
-        sec++;
-        console.log(sec);
-    }, 100)
+        msec++;
+        msecT.value = parseInt(msec) + 1;
+        console.log('oh boy', msec);
+        if (msec == 100) {
+            sec++;
+            msec = 0;
+            secT.value = parseInt(sec) + 1;
+        }
+        if (sec == 10) {
+            min++;
+            sec = 0;
+            minT.value = parseInt(min) + 1;
+        }
+        if (min == 5) {
+            hour++;
+            min = 0;
+            hourT.value = parseInt(hour) + 1;
+        }
+
+    }, 1)
 };
 
-function stop(){
-    clearInterval(timer, 100);
+function stop() {
+    clearInterval(timer, 1);
 }
 
-function clear(){
-    sec = 0;
+function clear() {
+    msecT.value = 0;
+    secT.value = 0;
+    minT.value =0;
+    hourT.value =0;
 }
-
-
-
 
 
 
