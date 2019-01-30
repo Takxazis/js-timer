9779482e3980ac643c968d1fa95f9c33b3f6aab7
AndreyKatrusha <andrey.katrusha@litslink.com>
2019-01-30 11:01:38 +0200

    add cheges

--- dz.js
+++ dz.js
@@ -1,20 +1,28 @@
-let msec = 0;
-let sec = 0;
-let min = 0;
-let hour = 0;
-
-
-
+//const element
 const Hstart = document.getElementById('HTMLstart').addEventListener("click", start);
 const Hclear = document.getElementById('HTMLclear').addEventListener("click", clear);
 const Hstop = document.getElementById('HTMLstop').addEventListener("click", stop);
 
-// T = text;
-
+// const time
 const hourT = document.getElementById('HTMLhour');
 const minT = document.getElementById('HTMLmin');
 const secT = document.getElementById('HTMLsec');
 const msecT = document.getElementById('HTMLmsec');
+const decimal = 10; // decemindal system
+const milisecond = 1000; // ritgh wey
+const minuteOrHour = 60 // 60  =) 
+
+// constanst first 
+// let variables
+let msec = 0;
+let sec = 0;
+let min = 0;
+let hour = 0;
+let timer = null; // need timer variable!!!
+
+
+
+
 
 
 
@@ -22,22 +30,22 @@ const msecT = document.getElementById('HTMLmsec');
 function start() {
     timer = setInterval(function () {
         msec++;
-        msecT.value = parseInt(msec) + 1;
+        msecT.value = parseInt(msec, decimal);
         console.log('oh boy', msec);
-        if (msec == 100) {
+        if (msec === milisecond) {
             sec++;
             msec = 0;
-            secT.value = parseInt(sec) + 1;
+            secT.value = parseInt(sec, decimal);
         }
-        if (sec == 10) {
+        if (sec === minuteOrHour) {
             min++;
             sec = 0;
-            minT.value = parseInt(min) + 1;
+            minT.value = parseInt(min, decimal);
         }
-        if (min == 5) {
+        if (min === minuteOrHour) {
             hour++;
             min = 0;
-            hourT.value = parseInt(hour) + 1;
+            hourT.value = parseInt(hour, decimal);
         }
 
     }, 1)
@@ -50,8 +58,12 @@ function stop() {
 function clear() {
     msecT.value = 0;
     secT.value = 0;
-    minT.value =0;
-    hourT.value =0;
+    minT.value = 0;
+    hourT.value = 0;
+    msec = 0;
+    sec = 0;
+    min = 0;
+    hour = 0;
 }
 
 
